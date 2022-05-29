527ddd5364a0b37ade9bbe2751de7f96fc89cea3
Luke Davis <lukelbd@gmail.com>
2022-05-29 13:08:05 -0600

    Rename theme to rtd_light_dark

--- rtd_light_dark/__init__.py
+++ rtd_light_dark/__init__.py
@@ -5,6 +5,9 @@ import os
 from pygments.formatters import HtmlFormatter
 
 # Declare themes. These should be kept updated with custom.js
+# Note the default them is only loaded before the javascript file can
+# read user preferences and toggle either the light or dark mode theme.
+init_theme = 'default'
 light_theme = 'pastie'
 dark_theme = 'monokai'
 
@@ -13,7 +16,7 @@ base = os.path.abspath(os.path.dirname(__file__))
 folder = os.path.join(base, 'static', 'pygments')
 if not os.path.isdir(folder):
     os.mkdir(folder)
-for style in (light_theme, dark_theme):  # or get_all_styles()
+for style in (init_theme, light_theme, dark_theme):  # or get_all_styles()
     file = os.path.join(folder, style + '.css')
     if os.path.isfile(file):
         continue
@@ -23,4 +26,4 @@ for style in (light_theme, dark_theme):  # or get_all_styles()
 # Add entrypoint for theme
 # See: https://www.sphinx-doc.org/en/master/development/theming.html
 def setup(app):  # noqa: E302
-    app.add_html_theme('rtd_minimal_theme', base)
+    app.add_html_theme('rtd_light_dark', base)
